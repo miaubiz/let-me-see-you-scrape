6696a7833021e887827611129bc752a964cca730
Michel Aubizzierre <miaubiz@gmail.com>
2012-01-17 11:19:00 +0200

    check for missing bugs

--- bugs.rb
+++ bugs.rb
@@ -1,11 +1,12 @@
 require 'open-uri'
-(68700..76000).each { |x|
+(75216..100000).each { |x|
     actual = x
     STDERR.puts "at #{actual}"
     open("https://bugs.webkit.org/show_bug.cgi?id=#{actual}")  { |f|
         f.each { |l| 
             puts l if /You are not authorized/=~l 
             STDERR.puts l if /You are not authorized/=~l
+            STDERR.puts "no more bugs: #{l}" if /Bug ##{actual} does not exist/=~l
         } 
     }
 }
